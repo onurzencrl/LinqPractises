d39abb8d2977f88756b3786975716eb4e666ac5f
onurz <onurz@DESKTOP-FVJLV9P>
2024-06-15 13:19:44 +0300

    IEntityTypeConfiguration konusu işlendi.

--- LinqPractises/Program.cs
+++ LinqPractises/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.Tracing;
+using System.Reflection;
 
 async void Main(string[] args)
 {
@@ -2478,6 +2480,130 @@ class ApplicationDbContext : DbContext
 
 #endregion
 
+#region IEntityTypeConfiguration
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+
+Console.WriteLine();
+
+#region OnModelCreating
+//Genel anlamda veritabanı ile ilgili konfigürasyonel operasyonların dışında entityler üzeirnde konfigürasyonel çalışmalar yapmamızı sağlayan bir fonskiyodundur.
+#endregion
+
+#region IEntityTypeConfiguration<T> Arayüzü
+//Entity bazlı yapılacak olan konfigürasyonları o entitye özel harici bir dosya üzerinde yapmamızı sağlayan bir arayüzdür.
+
+//Harici bir dosyada konfigürasyonların yürütülmesi merkezi bir yapılandırma noktası oluşturmamıızı sağlamaktadır.
+//Harici bir dosyada konfigüarsyonların yürültülmesi entity sayısının fazla olduğu senaryolarda yönetilebilirliği artturacak ve yapılandırma ile ilgili geliştiricinin yükünü azaltacaktır.
+#endregion
+
+#region ApplyConfiguration Metodu
+//Bu metot harici konfigürasyonel sınıflarımızı EF Core'a bildirebilmek için kullandığımız bir metotdur.
+#endregion
+
+#region ApplyConfigurationsFromAssembly Metodu
+//Uygulama bazında oluşturulan harici konfigürasyonel sınıfların her birini OnModelCreating metodunda ApplyCOnfiguration ile tek tek bildirmek yerine bu sınıfların bulunduğu Assembly'i bildirerek IEntityTypeConfiguration arayüzünden türeyen tüm sınıfları ilgili entitye karşılık konfigürasyonel değer olarak baz almasını tek kalemde gerçekleştirmemizi sağlayan bir metottur.
+#endregion
+
+class Order
+{
+    public int OrderId { get; set; }
+    public string Description { get; set; }
+    public DateTime OrderDate { get; set; }
+}
+
+class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.OrderId);
+        builder.Property(p => p.Description)
+            .HasMaxLength(13);
+        builder.Property(p => p.OrderDate)
+            .HasDefaultValueSql("GETDATE()");
+    }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.ApplyConfiguration(new OrderConfiguration());      
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!");
+    }
+}
+#endregion
+
+#region Seperated Configuration Classses
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+
+Console.WriteLine();
+
+#region OnModelCreating
+//Genel anlamda veritabanı ile ilgili konfigürasyonel operasyonların dışında entityler üzeirnde konfigürasyonel çalışmalar yapmamızı sağlayan bir fonskiyodundur.
+#endregion
+
+#region IEntityTypeConfiguration<T> Arayüzü
+//Entity bazlı yapılacak olan konfigürasyonları o entitye özel harici bir dosya üzerinde yapmamızı sağlayan bir arayüzdür.
+
+//Harici bir dosyada konfigürasyonların yürütülmesi merkezi bir yapılandırma noktası oluşturmamıızı sağlamaktadır.
+//Harici bir dosyada konfigüarsyonların yürültülmesi entity sayısının fazla olduğu senaryolarda yönetilebilirliği artturacak ve yapılandırma ile ilgili geliştiricinin yükünü azaltacaktır.
+#endregion
+
+#region ApplyConfiguration Metodu
+//Bu metot harici konfigürasyonel sınıflarımızı EF Core'a bildirebilmek için kullandığımız bir metotdur.
+#endregion
+
+#region ApplyConfigurationsFromAssembly Metodu
+//Uygulama bazında oluşturulan harici konfigürasyonel sınıfların her birini OnModelCreating metodunda ApplyCOnfiguration ile tek tek bildirmek yerine bu sınıfların bulunduğu Assembly'i bildirerek IEntityTypeConfiguration arayüzünden türeyen tüm sınıfları ilgili entitye karşılık konfigürasyonel değer olarak baz almasını tek kalemde gerçekleştirmemizi sağlayan bir metottur.
+#endregion
+
+class Order
+{
+    public int OrderId { get; set; }
+    public string Description { get; set; }
+    public DateTime OrderDate { get; set; }
+}
+
+class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.OrderId);
+        builder.Property(p => p.Description)
+            .HasMaxLength(13);
+        builder.Property(p => p.OrderDate)
+            .HasDefaultValueSql("GETDATE()");
+    }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.ApplyConfiguration(new OrderConfiguration());      
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!");
+    }
+}
+
+#endregion
 
 #region Code First Yaklaşımı
 /*
