87c3f37cc08df967da2ad7cf0952e983b9dc8ca5
onurz <onurz@DESKTOP-FVJLV9P>
2024-06-29 21:52:44 +0300

    Indexler , Sequencler  Explicit loading davranışı incelendi.

--- LinqPractises/Program.cs
+++ LinqPractises/Program.cs
@@ -3086,6 +3086,293 @@ class ApplicationDbContext : DbContext
 }
 #endregion
 
+#region Indexes
+
+ApplicationDbContext context = new();
+
+#region Index Nedir?
+//Index, bir sütuna dayalı sorgulamaları daha verimli ve performanslı hale getirmek için kullanılan yapıdır.
+#endregion
+#region Index'leme Nasıl Yapılır?
+//PK, FK ve AK olan kolonlar otomatik olarak indexlenir. 
+
+#region Index Attribute'u
+
+#endregion
+#region HasIndex Metodu
+
+#endregion
+#endregion
+#region Composite Index
+//context.Employees.Where(e => e.Name == "" || e.Surname == "")
+#endregion
+#region Birden Fazla Index Tanımlama
+
+#endregion
+#region Index Uniqueness
+
+#endregion
+#region Index Sort Order - Sıralama Düzeni (EF Core 7.0)
+
+#region AllDescending - Attribute
+//Tüm indexlemelerde descending davranışının bütünsel olarak konfigürasyonunu sağlar.
+#endregion
+#region IsDescending - Attribute
+//Indexleme sürecindeki her bir kolona göre sıralama davranışını hususi ayarlamak istiyorsak kullanılır.
+#endregion
+#region IsDescending Metodu
+
+#endregion
+#endregion
+#region Index Name
+
+#endregion
+#region Index Filter
+
+#region HasFilter Metodu
+
+#endregion
+#endregion
+#region Included Columns
+
+#region IncludeProperties Metodu
+
+#endregion
+#endregion
+
+
+
+//[Index(nameof(Name))]
+//[Index(nameof(Surname))]
+//[Index(nameof(Name), nameof(Surname))]
+//[Index(nameof(Name), AllDescending = true)]
+//[Index(nameof(Name), nameof(Surname), IsDescending = new[] { true, false })]
+//[Index(nameof(Name), Name = "name_index")]
+class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.Entity<Employee>()
+        //.HasIndex(x => x.Name);
+        //.HasIndex(x => new { x.Name, x.Surname });
+        //.HasIndex(nameof(Employee.Name), nameof(Employee.Surname));
+        //.HasIndex(x => x.Name)
+        //.IsUnique();
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .IsDescending();
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => new { x.Name, x.Surname })
+        //    .IsDescending(true, false);
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .HasDatabaseName("name_index");
+
+        //modelBuilder.Entity<Employee>()
+        //    .HasIndex(x => x.Name)
+        //    .HasFilter("[NAME] IS NOT NULL");
+
+        modelBuilder.Entity<Employee>()
+            .HasIndex(x => new { x.Name, x.Surname })
+            .IncludeProperties(x => x.Salary);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
+
+#endregion
+
+#region Sequences
+ApplicationDbContext context = new();
+
+#region Sequence Nedir?
+//Veritabanında benzersiz ve ardışık sayısal değerler üreten veritabanı nesnesidir.
+//Sequence herhangi bir tablonun özelliği değildir. Veritabanı nesnesidir. Birden fazla tablo tarafından kullanılabilir.
+#endregion
+#region Sequence Tanımlama
+//Sequence'ler üzerinden değer oluştururken veritabanına özgü çalışma yapılması zaruridir. SQL Server'a özel yazılan Sequence tanımı misal olarak Oracle için hata verebilir.
+
+#region HasSequence
+
+#endregion
+#region HasDefaultValueSql
+
+#endregion
+#endregion
+
+//await context.Employees.AddAsync(new() { Name = "Gençay", Surname = "Yıldız", Salary = 1000 });
+//await context.Employees.AddAsync(new() { Name = "Mustafa", Surname = "Yıldız", Salary = 1000 });
+//await context.Employees.AddAsync(new() { Name = "Tuaip", Surname = "Yıldız", Salary = 1000 });
+
+//await context.Customers.AddAsync(new() { Name = "Muiddin" });
+//await context.SaveChangesAsync();
+
+#region Sequence Yapılandırması
+
+#region StartsAt
+
+#endregion
+#region IncrementsBy
+
+#endregion
+#endregion
+#region Sequence İle Identity Farkı
+//Sequence bir veritabanı nesnesiyken, Identity ise tabloların özellikleridir.
+//Yani Sequence herhangi bir tabloya bağımlı değildir. 
+//Identity bir sonraki değeri diskten alırken Sequence ise RAM'den alır. Bu yüzden önemli ölçüde Identity'e nazaran daha hızlı, performanslı ve az maliyetlidir.
+#endregion
+
+class Employee
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+}
+class Customer
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasSequence("EC_Sequence")
+            .StartsAt(100)
+            .IncrementsBy(5);
+
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Id)
+            .HasDefaultValueSql("NEXT VALUE FOR EC_Sequence");
+
+        modelBuilder.Entity<Customer>()
+            .Property(c => c.Id)
+            .HasDefaultValueSql("NEXT VALUE FOR EC_Sequence");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
+#endregion
+
+#region Explicit Loading
+
+ApplicationDbContext context = new();
+#region Explicit Loading
+
+//Oluşturulan sorguya eklenecek verilerin şartlara bağlı bir şekilde/ihtiyaçlara istinaden yüklenmesini sağlayan bir yaklaşımdır.
+
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//if (employee.Name == "Gençay")
+//{
+//    var orders = await context.Orders.Where(o => o.EmployeeId == employee.Id).ToListAsync();
+//}
+
+#region Reference
+
+//Explicit Loading sürecinde ilişkisel olarak sorguya eklenmek istenen tablonun navigation propertysi eğer ki tekil bir türse bu tabloyu reference ile sorguya ekleyebilemkteyiz.
+
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+////...
+////...
+////...
+//await context.Entry(employee).Reference(e => e.Region).LoadAsync();
+
+//Console.WriteLine();
+#endregion
+#region Collection
+
+//Explicit Loading sürecinde ilişkisel olarak sorguya eklenmek istenen tablonun navigation propertysi eğer ki çoğul/koleksiyonel bir türse bu tabloyu Collection ile sorguya ekleyebilemkteyiz.
+
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//...
+//...
+//...
+//await context.Entry(employee).Collection(e => e.Orders).LoadAsync();
+
+//Console.WriteLine();
+#endregion
+
+#region Collection'lar da Aggregate Operatör Uygulamak
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//...
+//...
+//...
+//var count = await context.Entry(employee).Collection(e => e.Orders).Query().CountAsync();
+Console.WriteLine();
+#endregion
+#region Collection'lar da Filtreleme Gerçekleştirmek
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+////...
+////...
+////...
+//var orders = await context.Entry(employee).Collection(e => e.Orders).Query().Where(q => q.OrderDate.Day == DateTime.Now.Day).ToListAsync();
+#endregion
+#endregion
+
+public class Employee
+{
+    public int Id { get; set; }
+    public int RegionId { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Salary { get; set; }
+
+    public List<Order> Orders { get; set; }
+    public Region Region { get; set; }
+}
+public class Region
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+}
+public class Order
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime OrderDate { get; set; }
+
+    public Employee Employee { get; set; }
+}
+
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+    }
+}
+
+#endregion
+
 #region Code First Yaklaşımı
 /*
  * Code-First Approach Create Database from the Domain Classes
