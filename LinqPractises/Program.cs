b30e7ffae18a1df81149e326584a1d823de2b49e
onurz <onurz@DESKTOP-FVJLV9P>
2024-07-18 21:10:23 +0300

    Query Log çalışması yapıldı.

--- LinqPractises/Program.cs
+++ LinqPractises/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Logging;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.Tracing;
@@ -4772,6 +4773,70 @@ class ApplicationDbContext : DbContext
 
 #endregion
 
+
+#region Query Log
+
+ApplicationDbContext context = new();
+#region Query Log Nedir?
+//LINQ soprguları neticesinde generate edilen sorguları izleyebilmek ve olası teknik hataları ayıklayabilmek amacıyla query log mekanizmasından istifade etmekteyiz.
+#endregion
+#region Nasıl Konfigüre Edilir?
+//Microsoft.Extensions.Logging.Console
+
+await context.Persons.ToListAsync();
+
+await context.Persons
+   .Include(p => p.Orders)
+   .Where(p => p.Name.Contains("a"))
+   .Select(p => new { p.Name, p.PersonId })
+   .ToListAsync();
+#endregion
+#region Filtreleme Nasıl Yapılır?
+
+#endregion
+
+public class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; }
+}
+public class Order
+{
+    public int OrderId { get; set; }
+    public int PersonId { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+
+    public Person Person { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.Orders)
+            .WithOne(o => o.Person)
+            .HasForeignKey(o => o.PersonId);
+    }
+    readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder
+    .AddFilter((category, level) =>
+    {
+        return category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information;
+    })
+    .AddConsole());
+    protected override async void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+        optionsBuilder.UseLoggerFactory(loggerFactory);
+    }
+}
+#endregion
 
 #region Code First Yaklaşımı
 /*
